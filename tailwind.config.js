8e94d56b79cdad438e79daa975c6e76be14a236a
auwaldev-creator <auwalbashar12@gmail.com>
2025-12-30 17:01:49 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 15s ease infinite',
+        'typewriter': 'typewriter 2s steps(40) 1s 1 normal both',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%, 100%': { 'background-size': '200% 200%', 'background-position': 'left center' },
+          '50%': { 'background-size': '200% 200%', 'background-position': 'right center' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}

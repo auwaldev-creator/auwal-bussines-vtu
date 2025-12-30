defb3c51e9b9687a01e77229a4be2a9a328af5ab
auwaldev-creator <auwalbashar12@gmail.com>
2025-12-30 16:45:32 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Wannan layin shine yake jawo Tailwind din da mukasa a sama
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
